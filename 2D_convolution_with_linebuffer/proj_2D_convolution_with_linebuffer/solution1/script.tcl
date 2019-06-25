7d9f6b2346ed04a157dc9bb2578fcc3b301fa19d
kefuzheng <kefuzheng@hotmail.com>
2019-06-25 07:50:42 +0530

    test add to index and commit

--- 2D_convolution_with_linebuffer/proj_2D_convolution_with_linebuffer/solution1/script.tcl
+++ 2D_convolution_with_linebuffer/proj_2D_convolution_with_linebuffer/solution1/script.tcl
@@ -1,5 +1,5 @@
 ############################################################
-## This file is generated automatically by Vivado HLS.
+## This file is generated automatically by Scout HLS.
 ## Please DO NOT edit it.
 ## Copyright (C) 1986-2019 Xilinx, Inc. All Rights Reserved.
 ############################################################
@@ -12,6 +12,6 @@ set_part {xc7z020-clg484-1}
 create_clock -period 6.66 -name default
 source "./proj_2D_convolution_with_linebuffer/solution1/directives.tcl"
 csim_design
-csynth_design
+csynth_design -dump_cfg
 cosim_design
 export_design -format ip_catalog
